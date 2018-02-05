78f433a19ee35ccbdd3bac05e4b9521f3d4c16df
Srna Radic <srna6789@hotmail.com>
2018-02-05 01:03:31 +0100

    Scale effect is added, also when you click on any nav link it stays marked until you don't choose another one

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$('#all').on('click', function(){
+  $(this).css({"background-color" : "#c5e7cf", "text-align" : "center", "font-weight" : "bold"});
+  $('#webdesign').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#illustration').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#photo').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+});
+
+$('#webdesign').on('click', function(){
+  $(this).css({"background-color" : "#c5e7cf", "text-align" : "center", "font-weight" : "bold"});
+  $('#all').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#illustration').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#photo').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+});
+
+
+$('#illustration').on('click', function(){
+  $(this).css({"background-color" : "#c5e7cf", "text-align" : "center", "font-weight" : "bold"});
+  $('#webdesign').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#all').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#photo').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+});
+
+
+$('#photo').on('click', function(){
+  $(this).css({"background-color" : "#c5e7cf", "text-align" : "center", "font-weight" : "bold"});
+  $('#webdesign').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#illustration').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+  $('#all').css({"background-color" : "#ffff96", "font-weight" : "normal"});
+});
